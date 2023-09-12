addb51a1dd0eccd587088c7b9a7ed20682bce013
iamMusa <godwinobas0@gmail.com>
2023-09-12 15:38:15 +0100

    created an api.js file to handle routes

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require('express');
 // Setting up express app
 const app = express();
 
+app.get('/', function(){
+    console.log('GET Request');
+    res.send({});
+});
+
 // listen for requests
 app.listen(process.env.port || 4000, function(){
     console.log('listening for requests on port 4000')
