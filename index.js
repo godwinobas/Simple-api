aa7e0f924ffa6d23fcd340603993f87d73673e27
iamMusa <godwinobas0@gmail.com>
2023-09-14 20:34:29 +0100

    added spots schema and model

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const routes = require('./routes/api')
+const bodyParser = require('body-parser');
 
 // Setting up express app
 const app = express();
 
-app.use(routes);
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+// route handler
+app.use('/api', routes);
 
 // listen for requests
 app.listen(process.env.port || 4000, function(){
--- routes/api.js
+++ routes/api.js
@@ -2,22 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 // get a list of chilling spots from the db
-router.get('spots', function(req, res){
+router.get('/spots', function(req, res){
     res.send({type: 'GET'});
 });
 
 // add new chilling spot
-router.get('spots', function(req, res){
-    res.send({type: 'POST'});
+router.post('/spots', function(req, res){
+    console.log(JSON.stringify(req.body));
+    res.send({
+        type: 'POST',
+        name: req.body.name,
+        location: req.body.location
+    });
 });
 
 // update existing chilling spot
-router.get('spots/:id', function(req, res){
+router.put('/spots/:id', function(req, res){
     res.send({type: 'PUT'});
 });
 
 // delete chilling spot
-router.get('spots/:id', function(req, res){
+router.delete('/spots/:id', function(req, res){
     res.send({type: 'DELETE'});
 });
 
