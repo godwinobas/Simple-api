4ad565587e958bd5b808bddbea86deef2cdb7167
iamMusa <godwinobas0@gmail.com>
2023-09-16 03:36:20 +0100

    Added geolocation schema

--- models/spot.js
+++ models/spot.js
@@ -1,6 +1,18 @@
 const mongoose = require ('mongoose');
 const Schema = mongoose.Schema;
 
+// create geolocation Schema
+const GeoSchema = new Schema({
+    type: {
+        type: String,
+        default: "Point"
+    },
+    coordinates: {
+        type: [Number],
+        index: "2dsphere"
+    }
+});
+
 // create spots Schema & model
 const SpotSchema = new Schema({
     name: {
@@ -13,8 +25,8 @@ const SpotSchema = new Schema({
     available: {
         type: Boolean,
         default: false
-    }
-    // add in geo location
+    },
+    geometry: GeoSchema
 });
 
 const Spot = mongoose.model('spot', SpotSchema);
