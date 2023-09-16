e56e35d37651a7549de119e1779aa3aafdf496bc
iamMusa <godwinobas0@gmail.com>
2023-09-16 02:46:48 +0100

    added error handling

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ require('dotenv').config();
 const app = express();
 
 // connect to mongdb
+connectDatabase();
+
 async function connectDatabase(){
     await mongoose.connect(process.env.dbURI)
     .then
     console.log("Connected to database successfully")
 };
 
-connectDatabase();
+
 mongoose.Promise = global.Promise;
 
 app.use(bodyParser.urlencoded({
@@ -23,6 +25,12 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 // route handler
 app.use('/api', routes);
+
+// error handling middleware
+app.use(function(err, req, res, next){
+    console.log(err);
+    res.status(422).send({error: err.message});
+});
 
 // listen for requests
 app.listen(process.env.port || 4000, function(){
--- routes/api.js
+++ routes/api.js
@@ -1,28 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const Spot = require('../models/spot');
 
 // get a list of chilling spots from the db
-router.get('/spots', function(req, res){
+router.get('/spots', function(req, res, next){
     res.send({type: 'GET'});
 });
 
-// add new chilling spot
-router.post('/spots', function(req, res){
-    console.log(JSON.stringify(req.body));
-    res.send({
-        type: 'POST',
-        name: req.body.name,
-        location: req.body.location
-    });
+// add new chilling spot to db
+router.post('/spots', function(req, res, next){
+    Spot.create(req.body).then(function(spot){
+        res.send(spot);
+    }).catch(next);
 });
 
 // update existing chilling spot
-router.put('/spots/:id', function(req, res){
+router.put('/spots/:id', function(req, res, next){
     res.send({type: 'PUT'});
 });
 
 // delete chilling spot
-router.delete('/spots/:id', function(req, res){
+router.delete('/spots/:id', function(req, res, next){
     res.send({type: 'DELETE'});
 });
 
